88360361b417025129e73c8283c05ffbdd9267ed
akurok <vladkonovalov20060@gmail.com>
2024-03-31 03:11:17 +0500

    Добавил бд и подбравил textBox

---
+++ Model/ApplicationContext .cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoWPFX.Model
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<CryptoCurrency> CryptoCoin { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=coin.db");
+        }
+
+    }
+}
